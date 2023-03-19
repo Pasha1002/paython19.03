e79e985a23b9c55a368e533d39d7c6d87ae50142
Pasha1002 <kanavetspavel2001@gmail.com>
2023-03-19 13:23:40 +0200

    tusk dune

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,7 @@
 
+num_1 = int(input('Input number 1: '))
+num_2 = int(input('Input number 2: '))
+num_3 = int(input('Input number 3: '))
+num_4 = int(input('Input number 4: '))
+
+print (f'Resalt: {num_1*num_2*num_3*num_4}')
